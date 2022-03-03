bbbcb33407a096571d60f1db6ee2b8b08a34a75b
sgriffith3 <sgriffith@alta3.com>
2022-03-03 19:01:56 +0000

    updated app01

--- app01.py
+++ app01.py
@@ -10,12 +10,20 @@ This script shows the answer to the common question of what the following bit of
 import app02
 import app03
 
-print("My name is:")
-print(__name__)
 
-print("The llamas function from app02 says it has the __name__ of:")
-app02.llamas()
+def find_dem_names():
+    print("My __name__ is:")
+    print(__name__)
+    
+    print("The llamas function from app02 says it has the __name__ of:")
+    app02.llamas()
+    
+    print("The myname function from app03 says it has the __name__ of:")
+    app03.myname()
+    app03.tim()
 
-print("The myname function from app03 says it has the __name__ of:")
-app03.myname()
-app03.tim()
+
+if __name__ == '__main__':
+    find_dem_names()
+
+# Ctrl v (select block of chars) Shift i  (type your spaces in) Esc
